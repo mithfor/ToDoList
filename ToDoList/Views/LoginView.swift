f5d9ff1d75a7df51a244ff857548aaec751ca176
Mithfor <mithfor@gmail.com>
2023-05-05 11:24:14 +0300

    Implement Reusable HeaderView

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -16,7 +16,10 @@ struct LoginView: View {
         NavigationView {
             VStack {
                 // Header
-                HeaderView()
+                HeaderView(title: "To Do List",
+                           subtitle: "Get things done",
+                           angle: 15,
+                           background: .pink)
                 
                 // Login Form
                 Form {
@@ -39,6 +42,7 @@ struct LoginView: View {
                     }
                     .padding()
                 }
+                .offset(y: -50)
                 
                 // Create Account
                 VStack {
--- ToDoList/Views/RegisterView.swift
+++ ToDoList/Views/RegisterView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct RegisterView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HeaderView(title: "Register",
+                       subtitle: "Start organize todos",
+                       angle: -15,
+                       background: .orange)
+            
+            Spacer()
+        }
     }
 }
 
