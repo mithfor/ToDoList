aa7af8e61cb913de1ded498c512f31b42727b020
Mithfor <mithfor@gmail.com>
2023-05-05 07:32:13 +0300

    First

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Dmitrii Voronin on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
