813c9d6ef1b05049e8cece6adc53bc607fb7fdbb
Mithfor <mithfor@gmail.com>
2023-05-17 07:40:04 +0300

    Added LogoutView

--- ToDoList/Views/LogoutView.swift
+++ ToDoList/Views/LogoutView.swift
@@ -13,17 +13,55 @@ struct LogoutView: View {
     var body: some View {
         VStack {
             
-            Button(action: {
+            // Avatar
+            Image(systemName: AppConstants.TabBar.Images.profile)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(Color.blue)
+                .frame(width: 150, height: 150)
+            
+            // Info: Name, Email, Member since
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Name: ")
+                    Text("mithfor")
+                }
+                HStack {
+                    Text("Email: ")
+                    Text("mithfor")
+                }
+                HStack {
+                    Text("Member since: ")
+                    Text(Date().formatted(date: .abbreviated, time: .omitted))
+                }
+            }
+            
+            Spacer()
+                
+            // Sign out
+            
+            TLButton(title: "Log Out",
+                     background: .blue) {
                 let firebaseAuth = Auth.auth()
                 do {
                     try firebaseAuth.signOut()
                 } catch let signOutError as NSError {
                     print("Error signing out \(signOutError)")
                 }
-            }, label: {
-                Text("Sign Out")
-            })
-            .padding(.bottom, 50)
+            }
+                     .padding(.bottom, 50)
+            
+//            Button(action: {
+//                let firebaseAuth = Auth.auth()
+//                do {
+//                    try firebaseAuth.signOut()
+//                } catch let signOutError as NSError {
+//                    print("Error signing out \(signOutError)")
+//                }
+//            }, label: {
+//                Text("Sign Out")
+//            })
+//            .padding(.bottom, 50)
         }
     }
 }
